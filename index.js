116651cc77e3d9ccd36e6111d5dabd3c24e34c41
Mike McNeil <michael.r.mcneil@gmail.com>
2013-10-07 03:17:06 -0500

    Updates to allow for more fluent usage.

--- index.js
+++ index.js
@@ -10,17 +10,22 @@ module.exports = (function () {
 	// Collection configurations
 	var _collectionConfigs = {};
 
-	return {
+	var Adapter = {
 
 		// App-wide defaults for this adapter
 		defaults: {
-
+			from: {
+				name: 'Sails.js',
+				email: 'contact@yourapp.com'
+			}
 		},
 
 		registerCollection: function (collection, cb) {
+			// Absorb defaults into collection configuration
+			collection.config = _.defaults(collection.config, Adapter.defaults);
 
-			// Handle collection config
-			_collectionConfigs[collection.identity] = collection.defaults;
+			// Store each collection config for later
+			_collectionConfigs[collection.identity] = _.cloneDeep(collection.config);
 			cb();
 		},
 		teardown: function (cb) {
@@ -33,10 +38,9 @@ module.exports = (function () {
 		 *
 		 * @param {String} cid - [optional]
 		 *		Collection/Model identity or null
-		 *		TODO: Waterline core should set `cid` if called from model context
-		 *		TODO: Waterline core should automatically apply model config
 		 *
 		 * @param {Object} options - [optional]
+		 *		TODO: Waterline core should automatically apply model config to options
 		 *
 		 * @param {Function} cb - [optional]
 		 */
@@ -45,10 +49,20 @@ module.exports = (function () {
 			cb =	_.isFunction(cb) ? cb : 
 					_.isFunction(options) ? options :
 					function (){};
-			
 			options = _extendOptions(cid, options);
+
+			var err = _validateOptions(options);
+			if ( err ) return cb(err);
+
+			// Set-up API key
 			var mandrill = new Mandrill(options.apiKey);
 			delete options.apiKey;
+
+			// Tolerate rowdy inputs
+			options = _marshalOptions(options);
+
+			// Map everything to Mandrill's expectations
+			options = _serializeOptions(options);
 
 			mandrill.messages.send(options,
 			function success (result) {
@@ -59,6 +73,8 @@ module.exports = (function () {
 			});
 		}
 	};
+
+	return Adapter;
 
 
 
@@ -77,6 +93,66 @@ module.exports = (function () {
 			return _.extend({}, _collectionConfigs[cid], options);
 		}
 		return _.extend({}, options);
+	}
+
+	/**
+	 * @returns truthy value if options are invalid or incomplete
+	 * @api private
+	 */
+	function _validateOptions(options) {
+		if (!options.to) {
+			return '`to` must be specified, e.g.: { to: { email: "foo@bar.com", name: "Mr. Foo Bar" } }';
+		}
+		if (!options.from) {
+			return '`from` must be specified, e.g.: { from: { email: "foo@bar.com", name: "Mr. Foo Bar" } }';
+		}
+	}
+
+
+	/**
+	 * @returns {Object} options with types normalized for consistency
+	 * @api private
+	 */
+	function _marshalOptions(options) {
+		if ( _.isString(options.to) ) {
+			options.to = [ { email: options.to } ];
+		}
+		if ( _.isPlainObject(options.to) ) {
+			options.to = [ options.to ];
+		}
+		_.map(options.to, function (recipient) {
+			if ( _.isString(recipient) ) {
+				return { email: recipient };
+			}
+			return recipient;
+		});
+
+		if ( _.isString(options.from) ) {
+			options.from = { email: options.from };
+		}
+		return options;
+	}
+
+	/**
+	 * @returns {Object} options formatted for use w/ mandrill
+	 * @api private
+	 */
+	function _serializeOptions(options) {
+		_.merge(options, {
+			message: {
+				to: options.to,
+				html: options.html || '',
+				text: options.text || undefined,
+				from_email: options.from.email,
+				from_name: options.from.name
+			}
+		});
+		delete options.to;
+		delete options.from;
+		delete options.html;
+		delete options.text;
+
+		return options;
 	}
 
 })();
